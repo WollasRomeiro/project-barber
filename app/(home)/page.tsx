b8b2c30fe8dc25b4078208d18a8f534ea56a1fca
WollasRomeiro <wollasromeiro@icloud.com>
2024-02-02 00:12:27 -0300

    feat: add search components

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,6 +1,8 @@
 import { format } from "date-fns";
 import Header from "../_components/header";
 import { ptBR } from "date-fns/locale";
+import Search from "./_components/search";
+
 
 export default function Home() {
   return (
@@ -13,6 +15,9 @@ export default function Home() {
         locale: ptBR,
           })}
         </p>
+      </div>
+      <div className="px-5 mt-6">
+      <Search />
       </div>
     </div>
   )
