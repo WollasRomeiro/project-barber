2f957e42b071cd639f83d89da093cda5167668cc
WollasRomeiro <wollasromeiro@icloud.com>
2024-02-02 23:01:27 -0300

    feat: create components populares

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -30,6 +30,7 @@ export default async function Home() {
         </h2>
         <BookingItem />
       </div>
+
       <div className="mt-6">
         <h2 className="px-5 texte-xs mb-3 uppercase text-gray-400 font-bold">
           Recomendados
@@ -37,7 +38,19 @@ export default async function Home() {
 
         <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
           {barbershops.map((barbershop) => (
-           <BarberShopItem key={barbershop.id} barbershop={barbershop}/>
+            <BarberShopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
+      </div>
+
+      <div className="mt-6">
+        <h2 className="px-5 texte-xs mb-3 uppercase text-gray-400 font-bold">
+          Populares
+        </h2>
+
+        <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+          {barbershops.map((barbershop) => (
+            <BarberShopItem key={barbershop.id} barbershop={barbershop} />
           ))}
         </div>
       </div>
